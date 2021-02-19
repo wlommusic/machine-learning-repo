f275d7c7aebdb1a2e943ebdfb2d8bfc8f78a2b09
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-02-19 20:42:51 +0530

    update 2

---
+++ #3 classification/3 SVM/SVM.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 19:59:10 2021
+
+@author: wlom
+"""
+
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#spillting the data set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=0)
+
+# print(x_test)
+# print(y_test)
+
+#feature scaling
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+#training the SVM model
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear',random_state=0)
+classifier.fit(x_train,y_train)
+
+#Predicting a new result
+# print(classifier.predict(sc.transform([[30,870000]])))
+
+#Predicting the Test set results
+y_pred = classifier.predict(x_test)
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#making the consfusion matrix
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+#visualizing the results (training set)
+from matplotlib.colors import ListedColormap
+x_set, y_set = sc.inverse_transform(x_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 10, stop = x_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = x_set[:, 1].min() - 1000, stop = x_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#visualizing the test set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = sc.inverse_transform(x_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 10, stop = x_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = x_set[:, 1].min() - 1000, stop = x_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
