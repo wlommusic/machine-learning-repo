43203223df5ff570d61fe3ae49a55a8749ebfa0a
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-03-01 21:18:28 +0530

    update

--- #4 model selection templates/Machine Learning A-Z (Model Selection)/Classification/logistic_regression.py
+++ #4 model selection templates/Machine Learning A-Z (Model Selection)/Classification/logistic_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
+dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
---
+++ #5 Clustering/Hierarchical Clustering/Hierachial-clustering.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 11:56:53 2021
+
+@author: wlom
+"""
+
