bd45cd483728b70652da3deb97c0c9cb4729bed3
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-03-14 18:21:44 +0530

    updates

--- #8 NLP/natural_language_processing.py
+++ #8 NLP/natural_language_processing.py
@@ -25,7 +25,7 @@ for i in range(0, 1000):
   review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
   review = ' '.join(review)
   corpus.append(review)
-print(corpus)
+# print(corpus)
 
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
@@ -44,10 +44,25 @@ classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))
+
+#single preds
+new_review = "Please don't buy this untill and unless you really really REALLY want a S pen. Very bad quality front camera, clicks like a 7k phone. Selfies are all soft and smudgy however the back camera is superb. Then the fingerprint scanner, that's a different story all together. Started to fail every single time after 2days of purchasing. Dual sim Never works as when both sim cards are at play, this phone struggles real hard to provide you network. Overall a very avarage phone, and surely overpriced. This phone should have costed 22k max.... "
+new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+new_review = new_review.lower()
+new_review = new_review.split()
+ps = PorterStemmer()
+all_stopwords = stopwords.words('english')
+all_stopwords.remove('not')
+new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+new_review = ' '.join(new_review)
+new_corpus = [new_review]
+new_X_test = cv.transform(new_corpus).toarray()
+new_y_pred = classifier.predict(new_X_test)
+print(new_y_pred)
